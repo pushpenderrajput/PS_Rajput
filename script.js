6aa2955a2a8e42f583ad2c3e3a42440ca2a47e0a
Pushpender Rajput <pushpenderrajputsp@gmail.com>
2023-04-30 23:55:35 +0530

    New Update

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if(scrollY > 20){
+        if (scrollY > 20) {
             $('.navbar').addClass("sticky");
 
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");  
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
         }
-        else{
-            $('.scroll-up-btn').removeClass("show"); 
+        else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
     //Scroll Button
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
     })
     //Menu button
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active")
         $('.menu-btn i').toggleClass("active")
     });
 });
 //Typing Animation
-var typed = new Typed(".typing",{
-    strings: ["Web Developer","MERN Stack Developer","Programmer"],
+var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Python Developer", "Programmer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
-var typed = new Typed(".typing-2",{
-    strings: ["Web Developer","MERN Stack Developer","Programmer"],
+var typed = new Typed(".typing-2", {
+    strings: ["Web Developer", "Python Developer", "Programmer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
